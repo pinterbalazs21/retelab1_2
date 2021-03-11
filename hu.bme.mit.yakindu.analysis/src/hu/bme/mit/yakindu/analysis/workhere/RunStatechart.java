9e755660adfb04211ef717ea926400e471b51bc8
Pintér Balázs <37505930+pinterbalazs21@users.noreply.github.com>
2021-03-11 17:31:00 +0100

    3. feladat

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,14 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -12,21 +17,35 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
-	}
+		 ExampleStatemachine s = new ExampleStatemachine();
+		 s.setTimer(new TimerService());
+		 RuntimeService.getInstance().registerStatemachine(s, 200);
+		 s.init();
+		 s.enter();
+		 Scanner scanner = new Scanner(System.in);
+		 String str;
+		 while((str = scanner.nextLine())!=null){
+			 if(str.equals("black")) {
+				 s.raiseBlack();
+				 s.runCycle();
+				 print(s);
+			 }else if(str.equals("white")) {
+				 s.raiseWhite();
+				 s.runCycle();
+				 print(s);
+			 }else if(str.equals("start")) {
+				 s.raiseStart();
+				 s.runCycle();
+				 print(s);
+			 }else if(str.equals("exit")) {
+				 System.exit(0);
+			 }else {
+				 System.out.println("invalid command");
+			 }
+		 }
+		 
+		 }
+
 
 	public static void print(IExampleStatemachine s) {
 		System.out.println("W = " + s.getSCInterface().getWhiteTime());
