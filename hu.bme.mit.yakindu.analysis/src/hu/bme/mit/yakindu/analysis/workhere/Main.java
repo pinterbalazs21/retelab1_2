4d5b8ce73e5b451dd94052f03ebe602ae4149bcd
Pintér Balázs <37505930+pinterbalazs21@users.noreply.github.com>
2021-03-12 11:52:52 +0100

    4.feladat

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,8 +1,12 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.yakindu.base.types.Event;
+import org.yakindu.base.types.Property;
+import org.yakindu.sct.model.sgraph.Scope;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
 import org.yakindu.sct.model.sgraph.Transition;
@@ -25,6 +29,8 @@ public class Main {
 		
 		// Reading model
 		Statechart s = (Statechart) root;
+		//2.feladat:
+		/*
 		TreeIterator<EObject> iterator = s.eAllContents();
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
@@ -42,7 +48,40 @@ public class Main {
 						stateID++;
 					}
 				}
+		}*/
+		//4.feladat:
+		EList<Scope> scopes = s.getScopes();
+		System.out.println("public static void main(String[] args) throws IOException {");
+		System.out.println("	ExampleStatemachine s = new ExampleStatemachine();");
+		System.out.println("	s.setTimer(new TimerService());");
+		System.out.println("	RuntimeService.getInstance().registerStatemachine(s, 200);");
+		System.out.println("	s.init();\r\n" + "	s.enter();");
+		System.out.println("	Scanner scanner = new Scanner(System.in);\r\n" + "	String str;");
+		System.out.println("	while((str = scanner.nextLine())!=null){");
+		for(Scope scope : scopes) {
+			for(Event e : scope.getEvents()) {
+				if(!e.equals(scope.getEvents().get(0)))
+					System.out.println("		else if(str.equals(\"" + e.getName() + "\")) {");
+				else
+					System.out.println("		if(str.equals(\"" + e.getName() + "\")) {");
+				System.out.println("			s.raise" + e.getName() + "();\r\n" + 
+						"			s.runCycle();\r\n" + 
+						"			print(s);}");
+			}
 		}
+		System.out.println("		else if(str.equals(\"exit\")) {\r\n" + 
+				"			System.exit(0);\r\n" + 
+				"		}else {\r\n" + 
+				"			System.out.println(\"invalid command\");\r\n" + 
+				"}}}");
+		System.out.println("	public static void print(IExampleStatemachine s) {");
+		for(Scope scope :scopes) {
+			for(Property p : scope.getVariables()) {
+				System.out.println("		System.out.println(\"" + p.getName() + " = \" + s.getSCInterface().get" + p.getName() + "());");
+			}
+		}
+		System.out.println("}");
+		
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -17,38 +17,36 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-		 ExampleStatemachine s = new ExampleStatemachine();
-		 s.setTimer(new TimerService());
-		 RuntimeService.getInstance().registerStatemachine(s, 200);
-		 s.init();
-		 s.enter();
-		 Scanner scanner = new Scanner(System.in);
-		 String str;
-		 while((str = scanner.nextLine())!=null){
-			 if(str.equals("black")) {
-				 s.raiseBlack();
-				 s.runCycle();
-				 print(s);
-			 }else if(str.equals("white")) {
-				 s.raiseWhite();
-				 s.runCycle();
-				 print(s);
-			 }else if(str.equals("start")) {
-				 s.raiseStart();
-				 s.runCycle();
-				 print(s);
-			 }else if(str.equals("exit")) {
-				 System.exit(0);
-			 }else {
-				 System.out.println("invalid command");
-			 }
-		 }
-		 
-		 }
-
-
-	public static void print(IExampleStatemachine s) {
-		System.out.println("W = " + s.getSCInterface().getWhiteTime());
-		System.out.println("B = " + s.getSCInterface().getBlackTime());
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		Scanner scanner = new Scanner(System.in);
+		String str;
+		while((str = scanner.nextLine())!=null){
+			if(str.equals("Start")) {
+				s.raiseStart();
+				s.runCycle();
+				print(s);}
+			else if(str.equals("White")) {
+				s.raiseWhite();
+				s.runCycle();
+				print(s);}
+			else if(str.equals("Black")) {
+				s.raiseBlack();
+				s.runCycle();
+				print(s);}
+			else if(str.equals("exit")) {
+				System.exit(0);
+			}else {
+				System.out.println("invalid command");
+	}}}
+		public static void print(IExampleStatemachine s) {
+			System.out.println("WhiteTime = " + s.getSCInterface().getWhiteTime());
+			System.out.println("BlackTime = " + s.getSCInterface().getBlackTime());
 	}
+
 }
+
+
